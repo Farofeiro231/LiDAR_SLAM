3ce31c8421a93b31eb65c1161d4c671533e5bb6b
farofeiro231 <victoremanoel23@gmail.com>
2019-04-11 02:26:05 +0200

    One process is deleting the list before the other can act upon it

--- ransac_functions.py
+++ ransac_functions.py
@@ -8,6 +8,9 @@ MIN_SAMPLES = 2
 #   Function to extract the line represented by the set of points for each subset of rangings. We create an x base array to be able to do << Boolean indexing >>.
 
 def landmark_extraction(xPoints, yPoints):
+    while True:
+        print("Tamanho de xPoints: {}" .format(len(xPoints)))
+        print("Tamanho de yPoints: {}" .format(len(yPoints)))
     data =  np.column_stack([xPoints, yPoints])  # Inliers returns an array of True or False with inliers as True.
     print(data)
     model_robust, inliers = ransac(data, LineModelND, min_samples=MIN_SAMPLES, 
