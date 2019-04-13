f64f2119aad2f08163f41dd0890801c558401a56
farofeiro231 <victoremanoel23@gmail.com>
2019-04-13 15:41:04 +0200

    RANSAC and plotting working with motor speed = 250

--- functions.py
+++ functions.py
@@ -28,22 +28,16 @@ def config_plot(figure, lin=1, col=1, pos=1, mode="rectilinear"):
 
 
 #  Check if the code has set the flag to do the RANSAC or to clear all of the points acquired because there are less of them then the MIN_NEIGHBOORS
-def check_ransac(keyFlags, xInliers, yInliers, xList, yList):#, innerFlag):
+def check_ransac(keyFlags, xInliers, yInliers, xList):#, innerFlag):
     temp_x, temp_y = list(), list()
     while True:
         #print("I'm checking for the RANSAC")
         if keyFlags.get(True) and len(xList) > 2:
-            #innerFlag[0] = True
-            #a = len(xList)
-            #b = len(yList)
-            #print('tamanho de xList: {}' .format(a))
-            #print('tamanho de yList: {}' .format(b))
             temp_x, temp_y = ransac_functions.landmark_extraction(xList)#, yList, innerFlag)
             xInliers.put(temp_x)
             yInliers.put(temp_y)
         else:
             del xList[:]
-            #del yList[:]
 
 
 #  Here we empty the xInliers and yInliers queue to be able to plot their data latter
@@ -56,17 +50,14 @@ def get_inliers(xInliers, yInliers, xPlot, yPlot):
 
 
 #   Here I run the landmark_extraction code inside an indepent process
-def ransac_core(flags_queue, xPoints, yPoints, xInliers, yInliers):
-    xList, yList = list(), list()
-    #innerFlag = [False]
+def ransac_core(flags_queue, xPoints, xInliers, yInliers):
+    xList = list()
     temp_x, temp_y = 0., 0.
-    ransac_checking = threading.Thread(target=check_ransac, args=(flags_queue, xInliers, yInliers, xList, yList, ))#innerFlag))
+    ransac_checking = threading.Thread(target=check_ransac, args=(flags_queue, xInliers, yInliers, xList, ))#innerFlag))
     ransac_checking.start()
     try:
         while True:
-            #if not innerFlag[0]:
                 xList.append(xPoints.get(True))
-                #yList.append(yPoints.get(True))
     except KeyboardInterrupt:
         flags_queue.close()
         pass
@@ -77,7 +68,6 @@ def distance_between_measures(new_measure, old_measure):
     if new_measure != 0:
         #print("Nova medida x velha medida: {0:.2f} x {1:.2f}".format(new_measure[0][3], old_measure))
         distance = abs(new_measure[0][3] - old_measure)
-        #print("Calculated distance: {:+f}".format(distance))
     else:
         distance = DISTANCE_LIMIT + 10
     return distance
@@ -87,7 +77,7 @@ def scanning(my_q):
     range_finder = Lidar('/dev/ttyUSB0')  # initializes serial connection with the lidar
     nbr_tours = 0
     start_time = time.time()
-    iterator = range_finder.scan('express', max_buf_meas=False, speed=350)  # returns a yield containing each measure
+    iterator = range_finder.scan('express', max_buf_meas=False, speed=250)  # returns a yield containing each measure
     try:
         for measure in iterator:
             #print("medindo...")
@@ -106,17 +96,16 @@ def scanning(my_q):
 
 def plotting(my_q):#, keyFlags, theta, distance, xPoints, yPoints, xInliers, yInliers, x, y):
     keyFlags = mp.Queue()
-    xPoints, yPoints = mp.Queue(), mp.Queue()
+    xPoints = mp.Queue()
     xInliers, yInliers = mp.Queue(), mp.Queue()
     theta, distance = list(), list()
     xPlot, yPlot = list(), list()
     print("Valor de keyFlags: {}" .format(keyFlags))
     print("Valor de keyFlags: {}" .format(my_q))
     print("Valor de keyFlags: {}" .format(xPoints))
-    print("Valor de keyFlags: {}" .format(yPoints))
     flag = False
     
-    ransac_process = mp.Process(target=ransac_core, args=(keyFlags, xPoints, yPoints, xInliers, yInliers, ))
+    ransac_process = mp.Process(target=ransac_core, args=(keyFlags, xPoints, xInliers, yInliers, ))
     ransac_process.daemon = True  # exits the process as soon as the main program stops
     ransac_process.start()
 
@@ -135,7 +124,7 @@ def plotting(my_q):#, keyFlags, theta, distance, xPoints, yPoints, xInliers, yIn
     fig = Figure()
 
     ax = config_plot(fig, col=1, pos=1, mode='polar')#, mode="polar")
-    #ax1 = config_plot(fig, col=2, pos=1)
+    #ax1 = config_plot(fig, col=2, pos=2)
 
     graph = FigureCanvasTkAgg(fig, master=root)
     graph.get_tk_widget().pack(side="top", fill='both', expand=True)
@@ -144,7 +133,7 @@ def plotting(my_q):#, keyFlags, theta, distance, xPoints, yPoints, xInliers, yIn
         nonlocal keyFlags
         nonlocal flag
         nonlocal theta, distance
-        nonlocal xPoints, yPoints
+        nonlocal xPoints
         nonlocal xInliers, yInliers
         nonlocal xPlot, yPlot
         print("Estou na função tal... Valor de flag: {}" .format(flag))
@@ -195,8 +184,8 @@ def plotting(my_q):#, keyFlags, theta, distance, xPoints, yPoints, xInliers, yIn
                     #ax1.grid()
                     theta_array = np.array(theta, dtype="float")
                     distance_array = np.array(distance, dtype="float")
-                    #xMask = np.concatenate(xInliers, axis=0)
-                    #yMask = np.concatenate(yInliers, axis=0)
+                    #xMask = np.concatenate(xPlot, axis=0)
+                    #yMask = np.concatenate(yPlot, axis=0)
                     ax.scatter(theta_array, distance_array, marker="+", s=3)
                     #ax.scatter(x, y, marker="+", s=3)
                     #ax1.scatter(xMask, yMask, marker=".", color='r', s=5)
