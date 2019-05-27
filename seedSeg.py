8ee9c452ab99eb6990f875f3ea66144d88fc697b
farofeiro231 <victoremanoel23@gmail.com>
2019-05-27 15:59:16 +0200

    Created the seed expansion function. Need still to implement the overlapping lines merge function

--- seedSeg.py
+++ seedSeg.py
@@ -4,7 +4,7 @@ import re
 from skimage.measure import LineModelND
 
 SNUM = 6
-P = 10
+PMIN = 10
 P2L = 1
 P2P = 50
 
@@ -23,7 +23,34 @@ for item in temp:
 plt.show()
 
 
-
+# Here the seeds are expanded to form the landmarks. i is the point the seed begins.
+seed_expansion(lm, x, y, seed, N, i):
+    outlier = False
+    j = i + 7  # gets the next point, because the last seed's point is i + 6
+    k = i - 1
+    while not outlier and j < N:
+        nextPoint = np.array([[x[j], y[j]]])
+        if lm.residuals(nextPoint) < P2L:
+            seed = np.concatenate((seed, nextPoint), axis=0) # Inserts the point at the bottom of the current seed. Now we refit the model
+            lm.estimate(seed)
+            j += 1
+        else:
+            outlier = True
+    #j = j - 1
+    outlier = False
+    while not outlier and k >= 0:
+        previousPoint = np.array([[x[k], y[k]]])
+        if lm.residuals(previousPoint) < P2L:
+            seed = np.concatenate((previousPoint, seed), axis=0) # Inserts the point at the top of the current seed. Now we refit the model
+            lm.estimate(seed)
+            k -= 1
+        else:
+            outlier = False
+    k= k + 1
+    if len(seed) > PMIN:  # Only returns the line and the seed if the final line has a minimum point number greater than PMIN previously set
+        return lm, seed, j
+    else:
+        return 0, 0, j
 
 
 
@@ -34,6 +61,10 @@ if __name__ == "__main__":
     i = 0
     flag = True
     success = False
+    lines = []
+    expandedSeeds = []
+    tempLine = 0
+    tempSeed = 0
     
     temp = re.findall("x,y:\(([+-]?\d+\.\d*) ([+-]?\d+\.\d*)\)", fd.read())
     for item in temp:
@@ -44,15 +75,18 @@ if __name__ == "__main__":
     lm = LineModelND
 
     while i < N - SNUM:
-        temp = np.array([x[i:i+6],y[i:i+6]]).T
-        success = lm.estimate(temp)
+        seed = np.array([x[i:i+6],y[i:i+6]]).T
+        success = lm.estimate(seed)
         if success:
-            distancesPoint2Line = lm.residuals(temp)
+            distancesPoint2Line = lm.residuals(seed)
             if not all(dist <= P2L for dist in distancesPoint2Line):
                 flag = False
                 break
+        else:
+            i += 1
         if flag:  # It means that the seed is valid
-            
+           [tempLine, tempSeed, i] = seed_expansion(lm, x, y, seed, N, i)
+
 
     plt.scatter(x, y)
     plt.show()
